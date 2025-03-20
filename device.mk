e3dddc0cfca4acfd49119975bde53de4f40ce89e
PabloG <gpablo1@yahoo.com.ar>
2025-03-20 20:47:16 +0100

    Added XiaomiParts

--- device.mk
+++ device.mk
@@ -72,3 +72,6 @@ $(call inherit-product-if-exists, vendor/xiaomi/miui-apps/miui-apps.mk)
 
 # Viper4AndroidFX
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+
+# XiaomiParts
+$(call inherit-product, packages/apps/XiaomiParts/parts.mk)
