797c153ab9d4c58d6ecf5361b4c1bae2a44a7f45
PabloG <gpablo1@yahoo.com.ar>
2025-04-06 22:13:02 +0200

    Revert "Add Init configuration files and Disable collective Device administrators GMS components"
    This reverts commit c2f5c4c3a22d314761d5e570345fd897f48f92cf.

--- rootdir/bin/init.qcom.early_boot.sh
+++
@@ -1,4 +0,0 @@
-# Disable GMS components
-pm disable com.google.android.gms/com.google.android.gms.auth.managed.admin.DeviceAdminReceiver
-pm disable com.google.android.gms/com.google.android.gms.chimera.GmsIntentOperationService
-pm disable com.google.android.gms/com.google.android.gms.mdm.receivers.MdmDeviceAdminReceiver
